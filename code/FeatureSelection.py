9630c290c690216f643df8d7c05535d55ca654ee
royhachnochi <roy.hachnochi@gmail.com>
2020-10-22 10:00:17 +0000

    saved labels files

--- code/FeatureSelection.py
+++ code/FeatureSelection.py
@@ -3,15 +3,23 @@ import numpy as np
 
 # ======================================================================================================================
 if __name__ == '__main__':
-    features_filename = "./results/reviewer_classification/articles_features.csv"
-    labels_filename = ""  # TODO
-    n_features = 50
+    # features_filename = "./results/reviewer_classification/articles_features.csv"
+    # labels_filename = "./results/reviewer_classification/articles_labels.csv"
+    # n_features = 50
+    #
+    # X = np.loadtxt(features_filename, delimiter=",")
+    # y = np.loadtxt(labels_filename, delimiter=",")
 
-    X = np.loadtxt(features_filename, delimiter=",")
-    y = np.loadtxt(labels_filename, delimiter=",")
+    X = np.array([[0, 0, 0, 3],
+                  [1, 1, 0, 3],
+                  [0, 1, 0, 3],
+                  [1, 0, 2, 3],
+                  [1, 0, 0, 3]])
+    y = np.array([0, 0, 1, 1, 1])
+    n_features = 2
 
     r = relief.ReliefF(n_features=n_features)
     X_transformed = r.fit_transform(X, y)
-
+    print()
 
 
